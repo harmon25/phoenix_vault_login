9daae2b554e1198fd884cbace8476828b91e8bee
harmoN25 <nomraharmon@gmail.com>
2016-02-24 14:39:56 -0500

    add function to check if a token is valid

--- lib/phoenix_vault_login.ex
+++ lib/phoenix_vault_login.ex
@@ -1,12 +1,49 @@
 defmodule VaultLogin do
   import Poison, only: [encode!: 1, decode!: 1]
   @authEndPoint "/v1/auth/userpass/login/"
+  @testEndPoint "/v1/auth/token/lookup-self"
+  @errMsg {:err, "Invalid request, missing or invalid data." }
+  @token "747710e0-2387-47cf-f592-38908aac0732"
 
   def auth(username, password) do
     System.get_env("VAULT_ADDR")
     |> genAuthRequest(username, password)
     |> sendRequest
     |> authResponse
+  end
+
+  def checkToken(token) do
+    System.get_env("VAULT_ADDR")
+    |> genCheckRequest(token)
+    |> sendRequest
+    |> checkResponse
+  end
+  
+  defp genCheckRequest(vaultAddr, token) when is_bitstring(vaultAddr) and is_bitstring(token)  do
+      {:ok,
+      %{
+        method: :get,
+        url: vaultAddr <> @testEndPoint,
+        payload: "",
+        headers: [{"Content-Type", 'application/json'}, {"X-Vault-Token", token}]
+       }
+     }
+  end
+  defp genCheckRequest(vaultAddr, _) when is_nil(vaultAddr), do: {:err, "VAULT_ADDR Environment Variable not set"}
+  defp genCheckRequest(_, _token), do: {:err, "Invalid token"}
+
+  defp checkResponse({:ok, responseCode, _, clientRef}) do
+     case responseCode do
+      200 ->
+         {:ok, respBody} = :hackney.body(clientRef)
+         {:ok, decode!(respBody) |> Map.get("data") }
+      403 ->
+        @errMsg
+      400 ->
+        @errMsg
+      500 ->
+        @errMsg
+      end
   end
 
   defp genAuthRequest(vaultAddr, username, password) when is_bitstring(vaultAddr) and is_bitstring(username) and is_bitstring(password) do
@@ -36,12 +73,11 @@ defmodule VaultLogin do
     case responseCode do
       200 ->
         {:ok, respBody} = :hackney.body(clientRef)
-        authInfo = decode!(respBody) |> Map.get("auth")
-        {:ok, authInfo }
+        {:ok, decode!(respBody) |> Map.get("auth") }
       400 ->
-        {:err, "Invalid request, missing or invalid data." }
+        @errMsg
       500 ->
-        {:err, "Invalid request, missing or invalid data." }
+        @errMsg
     end
 
   end
